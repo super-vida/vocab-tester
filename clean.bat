1ed87c946aab059702e6dae22b08494d0417324a
Jan Vacek <jvacek@gpe.cz>
2018-06-25 10:11:38 +0200

    database added

---
+++ clean.bat
@@ -0,0 +1,2 @@
+javaw -cp vocab-tester-1.0.0-SNAPSHOT.jar cz.prague.vida.vocab.VocabTextCleaner B-RSZ-27.txt
+pause
---
+++ print.bat
@@ -0,0 +1,3 @@
+@echo on
+start javaw -cp vocab-tester-1.0.0-SNAPSHOT.jar cz.prague.vida.vocab.VocabPrinter B-RSZ-27.txt
+pause
