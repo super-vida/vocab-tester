17274c0a5f3769df82b800f14e4f7c30b1bd495b
Jan Vacek <jvacek@gpe.cz>
2019-01-18 16:42:53 +0100

    db udate

---
+++ src/main/java/cz/prague/vida/vocab/Main.java
@@ -0,0 +1,15 @@
+package cz.prague.vida.vocab;
+
+import javafx.application.Application;
+
+public class Main {
+
+	private Main() {
+		super();
+	}
+
+	public static void main(final String[] args) {
+		Application.launch(VocabTesterFX.class, args);
+	}
+
+}
